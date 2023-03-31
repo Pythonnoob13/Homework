b1daa9687c6079b574251f5722bf75681336fbe3
Pythonnoob13 <robomgeladze@yahoo.com>
2023-03-31 21:43:51 +0400

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,8 @@
+product = ['phone', 800, 'usb', 30, 'ups', 180, 'pc', 2500, 'desktop', 1500]
+for x in product:
+    print(x)
+
+product1 = {'phone': 800, 'usb': 30, 'ups': 180, 'pc': 2500, 'desktop': 1500}
+for key, value in product1.items():
+    print(key, value)
+
