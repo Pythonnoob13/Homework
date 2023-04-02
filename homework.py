a08e809f339dfa2ca587511e910e57309721e7fa
Pythonnoob13 <robomgeladze@yahoo.com>
2023-04-02 14:15:34 +0400

    Update homework.py

--- homework.py
+++ homework.py
@@ -2,6 +2,20 @@ product = ['phone', 800, 'usb', 30, 'ups', 180, 'pc', 2500, 'desktop', 1500]
 for x in product:
     print(x)
 
+
+footballers = [
+    ['Messi', '40m'],
+    ['Leao', '70m'],
+    ['Osihmen', '120m'],
+    ['Gavi', '100m'],
+    ['Khvicha', '180m']
+]
+for player in footballers:
+    print(player[0], player[1])
+
+
+
+
 product1 = {'phone': 800, 'usb': 30, 'ups': 180, 'pc': 2500, 'desktop': 1500}
 for key, value in product1.items():
     print(key, value)
